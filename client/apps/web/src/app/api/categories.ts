1fb8f754fc3569dd9d9ec8b5b6150b2c0897e240
ahnsv <ahnsv@bc.edu>
2024-04-01 23:41:15 +0900

    feat(client/web): filter out empty string

--- client/apps/web/src/app/api/categories.ts
+++ client/apps/web/src/app/api/categories.ts
@@ -2,6 +2,6 @@ import { Database } from "@/lib/database.types";
 import { SupabaseClient } from "@supabase/supabase-js";
 
 export async function getCategories(supabaseClient: SupabaseClient, orgagnizationKey: string) {
-  return await supabaseClient.from("knowledge_categories").select("*").eq("org_key", orgagnizationKey);
+  return await supabaseClient.from("knowledge_categories").select("*").eq("org_key", orgagnizationKey).neq("name", "");
 }
 
