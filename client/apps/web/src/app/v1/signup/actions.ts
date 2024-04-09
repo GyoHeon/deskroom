5d5d46acd75f4a8fd7732b157b4a9ea11fc33a69
ahnsv <ahnsv@bc.edu>
2024-04-09 15:10:19 +0900

    feat(web): add to organization on signup

--- client/apps/web/src/app/v1/signup/actions.ts
+++ client/apps/web/src/app/v1/signup/actions.ts
@@ -25,7 +25,49 @@ export async function signUp(prevState: SignUpState, formData: FormData) {
       },
     }
   )
-  // TODO: add user to organization
+  // TODO: extract to a function
+  // TODO: move this after the user has been created
+  const orgEngName = formData.get('org-eng-name') as string
+  const orgKorName = formData.get('org-name') as string
+
+  if (!orgEngName || !orgKorName) {
+    return {
+      errors: "조직명을 입력해주세요.",
+      status: 400,
+    }
+  }
+
+  const { data: existingOrganization, error: existingOrganizationError } = await supabase.from('organizations').select('*').eq('key', orgEngName).single()
+
+  if (!existingOrganization && !!existingOrganizationError) {
+    const { data: newOrganization, error: createOrganizationError } = await supabase.from('organizations').insert([{
+      name_kor: orgKorName,
+      name_eng: orgEngName,
+      key: orgEngName,
+    }]).select().single()
+    if (!!createOrganizationError) {
+      return {
+        errors: createOrganizationError.message,
+        status: 400,
+      }
+    }
+
+    const { error: userOrganizationError } = await supabase.from('user_organizations').insert([{ user_id: data.user.id, org_id: newOrganization?.key }])
+
+    if (!!userOrganizationError) {
+      return {
+        errors: userOrganizationError.message,
+        status: 400,
+      }
+    }
+  }
+
+
+  const { error: userOrganizationError } = await supabase.from('user_organizations').insert([{ user_id: data.user.id, org_id: existingOrganization?.key }])
+
+  if (!!userOrganizationError) {
+    throw userOrganizationError
+  }
 
   if (error || !data?.user) {
     return {
