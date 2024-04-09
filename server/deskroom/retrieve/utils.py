1fd9de089006de8aaa1519f746721f2446eafddd
shawnhyeonsoo <shawn.khs1208@gmail.com>
2024-04-09 15:25:32 +0900

    async fix

--- server/deskroom/retrieve/utils.py
+++ server/deskroom/retrieve/utils.py
@@ -1,13 +1,11 @@
-from openai.types.chat import ChatCompletion
-
 from deskroom.common.openai import create_azure_openai_client, read_prompt
 
 
 async def retrieve_qns(policy: str, qa_db: str, user_qn: str) -> str | None:
     prompt = read_prompt(txt_file_from_prompts_dir="retrieval_prompt.txt")
-    openai_client = create_azure_openai_client()
+    openai_client = create_azure_openai_client(asynchronous=True)
 
-    chat_completion: ChatCompletion = await openai_client.chat.completions.create(
+    chat_completion = await openai_client.chat.completions.create(
         model="gpt-4-1106-preview",
         messages=[
             {
