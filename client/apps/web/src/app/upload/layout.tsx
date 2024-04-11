6755b491c7d5ad8664fd31edb9b96c3f38482e96
ahnsv <ahnsv@bc.edu>
2024-04-11 18:00:09 +0900

    feat(web): apply layout

---
+++ client/apps/web/src/app/upload/layout.tsx
@@ -0,0 +1,19 @@
+import TopNav from "@/components/TopNav"
+import { Box, Container, Flex } from "@radix-ui/themes"
+
+export default function UploadLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Flex direction={`column`}>
+      <TopNav />
+      <Container className="px-16 py-4 bg-primary-100 min-h-[800px]">
+        <Box className="rounded-xl bg-white p-5">
+          {children}
+        </Box>
+      </Container>
+    </Flex>
+  )
+}
--- client/apps/web/src/app/upload/success/page.tsx
+++ client/apps/web/src/app/upload/success/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default async function SuccessPage() {
   return (
-    <Flex direction={`column`}>
+    <Flex direction={`column`} align={`center`} justify={`center`}>
       <Image src="/deskroom-icon.png" alt="Deskroom Logo" width={60} height={60} className="my-8" />
       <Heading className="title" >데스크룸에 오신 것을 환영합니다.</Heading>
       <Text size="2" className="my-1 font-thin">로그인 후 데스크룸을 시작하세요.</Text>
