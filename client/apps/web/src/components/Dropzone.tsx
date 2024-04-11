ac9d38ca6b51a07f71123afed81854116c5c7353
ahnsv <ahnsv@bc.edu>
2024-04-12 01:13:50 +0900

    feat(web): style dropzone

--- client/apps/web/src/components/Dropzone.tsx
+++ client/apps/web/src/components/Dropzone.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Flex, Grid, Text } from "@radix-ui/themes";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { Box, Flex, Grid, Text } from "@radix-ui/themes";
 
 import { useRef, useState } from "react";
 
@@ -9,10 +10,12 @@ export type DropzoneProps = {
   id: string;
   name: string;
   multiple?: boolean;
-} & React.HTMLProps<HTMLDivElement>;
-export default function Dropzone({ heading = "파일을 업로드 해주세요.", id, name, multiple = false }: DropzoneProps) {
+} & React.HTMLAttributes<HTMLDivElement>;
+export default function Dropzone({ heading = "파일을 업로드 해주세요.", id, name, multiple = false, className }: DropzoneProps) {
   const [isDragging, setIsDragging] = useState(false);
+  const [files, setFiles] = useState<FileList | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+
 
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
@@ -36,39 +39,6 @@ export default function Dropzone({ heading = "파일을 업로드 해주세요."
     event.preventDefault();
     event.stopPropagation();
     setIsDragging(false);
-
-    const files = event.dataTransfer.files;
-    const file = files[0];
-
-    if (
-      file &&
-      file.type ===
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    ) {
-      alert("Not implemented");
-      throw new Error("Not implemented");
-
-      // Upload the file to the server
-      const formData = new FormData();
-      formData.append("file", file);
-
-      // Make a POST request to the server with the formData
-      fetch("/upload", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => {
-          // Handle the response from the server
-          // TODO: when uploaded, show a toast message
-          console.log(response);
-        })
-        .catch((error) => {
-          // Handle any errors
-          console.error(error);
-        });
-    } else {
-      console.log("Invalid file format. Please upload an xlsx file.");
-    }
   };
 
   const handleClick = () => {
@@ -76,15 +46,21 @@ export default function Dropzone({ heading = "파일을 업로드 해주세요."
       fileInputRef.current.click();
     }
   };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFiles(event.target.files);
+    }
+  }
 
   return (
     <Grid
-      className="dropzone my-2 h-[100px] place-items-center border-2 border-dashed border-violet3 bg-violet1 rounded-[12px] cursor-pointer transition-all ease-linear duration-75"
+      className={`dropzone my-2 h-[100px] place-items-center border-2 border-dashed border-violet3 bg-violet1 rounded-[12px] cursor-pointer transition-all ease-linear duration-75`}
       style={{
         border: isDragging
           ? "2px dashed var(--accent-a8)"
-          : "1px dashed var(--accent-a6)",
-        backgroundColor: "var(--accent-a1)",
+          : (!!files ? "1px solid var(--accent-a6)"
+            : "1px dashed var(--accent-a6)"),
+        backgroundColor: !!files ? "var(--accent-a6)" : "var(--gray-a1)",
         borderRadius: "var(--radius-3)",
       }}
       onDragOver={handleDragOver}
@@ -94,16 +70,28 @@ export default function Dropzone({ heading = "파일을 업로드 해주세요."
       onClick={handleClick}
     >
       <Flex direction="column" align="center">
-        {heading && (
-          <Text weight="bold" size="2">
-            {heading}
-          </Text>
-        )}
-        <Text color="gray" size={heading ? "2" : undefined}>
-          {isDragging
-            ? "Drop the (xlsx, csv) file here"
-            : "Drag and drop an (xlsx, csv) file here"}
-        </Text>
+        {
+          !files ? (
+            <>
+              <Text weight="bold" size="2">
+                {heading}
+              </Text>
+              <Text color="gray" size={heading ? "2" : undefined}>
+                {isDragging
+                  ? "Drop the (xlsx, csv) file here"
+                  : "Drag and drop an (xlsx, csv) file here"}
+              </Text>
+            </>
+          )
+            :
+            <Flex gap="2">
+              {Array.from(files).map((file, index) => (
+                <Box key={index} className="rounded border p-2 bg-primary-700 text-white">
+                  {file.name}
+                </Box>
+              ))}</Flex>
+
+        }
       </Flex>
       <input
         id={id}
@@ -113,6 +101,7 @@ export default function Dropzone({ heading = "파일을 업로드 해주세요."
         ref={fileInputRef}
         accept=".xlsx"
         multiple={multiple}
+        onChange={handleChange}
       />
     </Grid>
   );
