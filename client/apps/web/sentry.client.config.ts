81019327afa12212f877f6edbc306d286aff82a2
ahnsv <ahnsv@bc.edu>
2024-04-14 13:24:34 +0900

    feat(web): setup sentry

---
+++ client/apps/web/sentry.client.config.ts
@@ -0,0 +1,21 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 1,
+  debug: process.env.NODE_ENV !== 'production',
+  environment: process.env.NODE_ENV,
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  integrations: [
+    Sentry.replayIntegration({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+});
