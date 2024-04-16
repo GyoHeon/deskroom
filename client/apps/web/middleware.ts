5b88601a6fd309e3cd2b7b54ad98ca5814757368
ahnsv <ahnsv@bc.edu>
2024-04-16 16:59:05 +0900

    feat(web): apply server-side auth

--- client/apps/web/middleware.ts
+++ client/apps/web/middleware.ts
@@ -1,33 +1,10 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
-import type { Database } from '@/lib/database.types'
-
-const authCheckWhitelist = [
-  '/v1/login',
-  '/v1/signup',
-  '/v1/reset-password',
-  '/v1/reset-password/new',
-]
+import { updateSession } from '@/utils/supabase/middleware'
 
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-
-  const supabase = createMiddlewareClient<Database>({ req, res })
-  const { data: session } = await supabase.auth.getSession()
-
-  if (authCheckWhitelist.includes(req.nextUrl.pathname)) {
-    return res
-  }
-
-  if (!session) {
-    return NextResponse.redirect('/v1/login')
-  }
-
-
-  return res
+  return await updateSession(req)
 }
 
 // Ensure the middleware is only called for relevant paths.
@@ -39,6 +16,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico|v1).*)',
   ],
 }
