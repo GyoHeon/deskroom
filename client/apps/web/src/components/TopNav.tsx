53529008bf4aea4616322391d118689ab0ab3fe1
ahnsv <ahnsv@bc.edu>
2024-04-20 13:50:07 +0900

    refactor(web): remove unused vars

--- client/apps/web/src/components/TopNav.tsx
+++ client/apps/web/src/components/TopNav.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { useOrganizationContext } from "@/contexts/OrganizationContext";
-import { Avatar, DropdownMenu, Flex, Select } from "@radix-ui/themes";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import Image from "next/image";
 import { createClient } from "@/utils/supabase/client";
+import { Avatar, DropdownMenu, Flex, Select } from "@radix-ui/themes";
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export type TopNavProps = {
   shouldShowLogo?: boolean;
